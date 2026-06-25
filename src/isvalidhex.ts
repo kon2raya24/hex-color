afd7df9bfbf6d73c99b0f632b2007962381a988a
kon2raya24 <konturaya@gmail.com>
2026-06-25 13:41:31 +0000

    docs: add JSDoc comments to isvalidhex.ts

--- src/isvalidhex.ts
+++ src/isvalidhex.ts
@@ -1,3 +1,7 @@
+/**
+ * isValidHex
+ * @param hex
+ */
 export function isValidHex(hex: string): boolean {
   if (hex === null || hex === undefined) throw new Error("Invalid input");
   return /^#?([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/.test(hex);
